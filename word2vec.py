52d5180a11bf071b9eeceeaa520fa76f44c424cd
C0DET1GER <109345562+C0DET1GER@users.noreply.github.com>
2026-06-07 21:29:31 -0400

    word2vec update with adahessian

--- word2vec.py
+++ word2vec.py
@@ -15,7 +15,7 @@ def _():
         letters_only = ''.join(char for char in low if char in valid)
         return letters_only
 
-    txt = getText()[:10000000]
+    txt = getText()[:5000000]
     print(txt[:500])
     return (txt,)
 
@@ -24,7 +24,7 @@ def _():
 def _(txt):
     # tokenize and build vocab
     from collections import Counter
-    vocab_sz = 2000
+    vocab_sz = 3000
     stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
     # stop_words = []
     lst = [word for word in txt.split() if word != '']
@@ -113,6 +113,7 @@ def _(Neur, X_train, Y_train, np, vocab_sz, window):
                 loss_layer.save_D = Y_cur.T
                 loss = N_a.forward(X_cur.T)
                 total_loss += loss
+                history.append(loss)
                 grads, in_grad = N_a.backward(None)
                 history_00.append(grads[0])
                 params = N_a.get_params()
@@ -120,14 +121,13 @@ def _(Neur, X_train, Y_train, np, vocab_sz, window):
 
                 N_a.set_params(new_params)
 
-                if i * batch % 101 == 0:
+                if i % 50 == 0:
                     batch_loop.set_postfix({"loss": f"{loss:.2f}"})
 
                 batches += 1
 
             avg_loss = total_loss / batches
-            history.append(avg_loss)
-            # print(f"=> Average Loss: {avg_loss:.4f}")
+            # history.append(avg_loss)
 
             lr *= decay
 
@@ -170,6 +170,7 @@ def _(Neur, X_train, Y_train, get_cbow, get_one_hot, np, sz, tqdm, vocab_sz):
                 loss_layer.save_D = Y_cur.T
                 lv = N_a.forward(X_cur.T)
                 total_loss += lv
+                history.append(lv)
                 g, in_grad = N_a.backward(None)
                 history_00.append(g[0])
 
@@ -191,14 +192,14 @@ def _(Neur, X_train, Y_train, get_cbow, get_one_hot, np, sz, tqdm, vocab_sz):
                 rk = np.dot(d, g) / (2 * nrm2 * Lk)
                 N_a.set_params(p - ell * rk * d)
 
-                if i * batch % 101 == 0:
+                if i % 50 == 0:
                     batch_loop.set_postfix({"loss": f"{lv:.2f}"})
 
                 k += 1
                 batches += 1
 
             avg_loss = total_loss / batches
-            history.append(avg_loss)
+            # history.append(avg_loss)
 
         return N, history, history_00
 
@@ -207,7 +208,79 @@ def _(Neur, X_train, Y_train, get_cbow, get_one_hot, np, sz, tqdm, vocab_sz):
 
 @app.cell
 def _(Neur, X_train, Y_train, get_cbow, get_one_hot, np, sz, tqdm, vocab_sz):
+    def build_net_adahessian(dim, lr, epochs, batch=128):
+        history = []
+        history_00 = []
+
+        N = Neur.Network([Neur.Dense(vocab_sz, dim), Neur.Dense(dim, vocab_sz)])
+        loss_layer = Neur.Ilogit_and_KL(None)
+        N_a = Neur.Network([N, loss_layer])
+        b1, b2, e, eps = 0.9, 0.999, 1e-4, 1e-4
+
+        m = np.zeros(N_a.nb_params)
+        v = np.zeros(N_a.nb_params)
+        k = 0
+
+        for epoch in range(epochs):
+            total_loss = 0
+            indices = np.arange(sz)
+            np.random.shuffle(indices)
+            X_shuffled = X_train[indices]
+            Y_shuffled = Y_train[indices]
+
+            batch_loop = tqdm(range(0, len(X_shuffled), batch),
+                              desc=f"Epoch {epoch + 1}/{epochs}",
+                              unit="batch")
+            batches = 0
+
+            for i in batch_loop:
+
+                X_cur = get_cbow(X_shuffled[i:i+batch])
+                Y_cur = get_one_hot(Y_shuffled[i:i+batch])
+
+                loss_layer.save_D = Y_cur.T
+                lv = N_a.forward(X_cur.T)
+                total_loss += lv
+                history.append(lv)
+                g, in_grad = N_a.backward(None)
+                history_00.append(g[0])
+
+                p = N_a.get_params()
+                z = np.random.choice([-1.0, 1.0], size=len(p))
+                N_a.set_params(p + eps * z)
+                loss_layer.save_D = Y_cur.T
+                N_a.forward(X_cur.T)
+                gp, _ = N_a.backward(None)
+
+                N_a.set_params(p)
+                D = z * (gp - g) / eps
+                D = np.clip(D, -2.0, 2.0)
+
+                m = b1 * m + (1 - b1) * g
+                v = b2 * v + (1 - b2) * (D**2)
+                mh = m / (1 - b1 ** (k + 1))
+                vh = v / (1 - b2 ** (k + 1))
+
+                N_a.set_params(N_a.get_params() - lr * mh / (np.sqrt(vh) + e))
+
+                if i % 50 == 0:
+                    batch_loop.set_postfix({"loss": f"{lv:.2f}"})
+
+                k += 1
+                batches += 1
+
+            avg_loss = total_loss / batches
+            # history.append(avg_loss)
+
+        return N, history, history_00
+
+    return (build_net_adahessian,)
+
+
+@app.cell
+def _(Neur, X_train, Y_train, get_cbow, get_one_hot, np, sz, tqdm, vocab_sz):
     def build_net_adam(dim, lr, epochs, batch=128):
+        print(batch)
         history = []
         history_00 = []
 
@@ -240,6 +313,7 @@ def _(Neur, X_train, Y_train, get_cbow, get_one_hot, np, sz, tqdm, vocab_sz):
                 loss_layer.save_D = Y_cur.T
                 lv = N_a.forward(X_cur.T)
                 total_loss += lv
+                history.append(lv)
                 g, in_grad = N_a.backward(None)
                 history_00.append(g[0])
 
@@ -250,14 +324,14 @@ def _(Neur, X_train, Y_train, get_cbow, get_one_hot, np, sz, tqdm, vocab_sz):
 
                 N_a.set_params(N_a.get_params() - lr * mh / (np.sqrt(vh) + e))
 
-                if i * batch % 101 == 0:
+                if i % 50 == 0:
                     batch_loop.set_postfix({"loss": f"{lv:.2f}"})
 
                 k += 1
                 batches += 1
 
             avg_loss = total_loss / batches
-            history.append(avg_loss)
+            # history.append(avg_loss)
 
         return N, history, history_00
 
@@ -265,75 +339,62 @@ def _(Neur, X_train, Y_train, get_cbow, get_one_hot, np, sz, tqdm, vocab_sz):
 
 
 @app.cell
-def _(build_net_adam, build_net_red, build_net_sgd):
+def _(build_net_adahessian, build_net_adam, build_net_red, build_net_sgd):
     # Run and plot
     import matplotlib.pyplot as plt
     import itertools
-
-    """
-    dims_test = [50]
-    lr_test = [0.1]
-    epochs = [10]
-    decays = [1.0]
-    batches = [sz,256,8]
-    """
+    import time
 
     N_list = []
     hist_list = []
     hist2_list = []
+    time_list = []
 
-    N, history, history2 = build_net_adam(50, 0.005, 10, 64)
+    start_time = time.time()
+    N, history, history2 = build_net_adahessian(50, 0.01, 10, 64)
     N_list.append(N)
     hist_list.append(history)
     hist2_list.append(history2)
+    time_list.append(time.time() - start_time)
 
+    start_time = time.time()
+    N, history, history2 = build_net_adam(50, 0.01, 10, 64)
+    N_list.append(N)
+    hist_list.append(history)
+    hist2_list.append(history2)
+    time_list.append(time.time() - start_time)
+
+    start_time = time.time()
     N, history, history2 = build_net_red(50, 10, 64)
     N_list.append(N)
     hist_list.append(history)
     hist2_list.append(history2)
+    time_list.append(time.time() - start_time)
 
-    N, history, history2 = build_net_sgd(50, 0.05, 10, 1.0, 64)
+    start_time = time.time()
+    N, history, history2 = build_net_sgd(50, 1.0, 10, 1.0, 64)
     N_list.append(N)
     hist_list.append(history)
     hist2_list.append(history2)
-
-    """
-    for dim, lr, epoch, decay, batch in itertools.product(dims_test, lr_test, epochs, decays, batches):
-
-        print(f"Dimensions: {dim}, Learning Rate: {lr}, Epochs: {epoch}, Decay: {decay}, Batch Size: {batch}")
-        N, history, history2 = build_net(dim, lr, epoch, decay, batch)
-
-        plt.plot(range(1, epoch+1), history, marker='o', color='b')
-        plt.xlabel('Epoch')
-        plt.ylabel('Loss')
-        plt.grid(True)
-        plt.show()
-
-        plt.plot(history2, marker='', color='b')
-        plt.xlabel('Iteration')
-        plt.ylabel('W[0][0]')
-        plt.grid(True)
-        plt.show()
-
-        N_list.append(N)
-        hist_list.append(history)
-        hist2_list.append(history2)
-    """
+    time_list.append(time.time() - start_time)
     return N_list, hist2_list, hist_list, plt
 
 
 @app.cell
 def _(hist2_list, np, plt):
-    data_adam = hist2_list[0]
-    data_red = hist2_list[1]
-    data_sgd = hist2_list[2]
+    data_adahessian = hist2_list[0]
+    data_adam = hist2_list[1]
+    data_red = hist2_list[2]
+    data_sgd = hist2_list[3]
 
     x_adam = np.linspace(0, 10, len(data_adam))
+    x_adahessian = np.linspace(0, 10, len(data_adahessian))
     x_red = np.linspace(0, 10, len(data_red))
     x_sgd = np.linspace(0, 10, len(data_sgd))
 
-    plt.plot(x_adam, data_adam, color='g', alpha=0.3, label='Adam')
-    plt.plot(x_red, data_red, color='r', alpha=0.5, label='RED')
+    plt.plot(x_adam, data_adam, color='g', alpha=0.2, label='Adam')
+    plt.plot(x_adahessian, data_adahessian, color='k', alpha=0.2, label='AdaHessian')
+    plt.plot(x_red, data_red, color='r', alpha=0.2, label='RED')
     plt.plot(x_sgd, data_sgd, color='b', alpha=0.2, label='SGD')
 
     plt.xlabel('Epoch')
@@ -345,11 +406,36 @@ def _(hist2_list, np, plt):
 
 
 @app.cell
-def _(hist_list, plt):
-    # plt.plot(range(1, 10+1), hist_list[0], marker='o', color='k', label='GD')
-    plt.plot(range(1, 10+1), hist_list[0], marker='o', color='r', label='Adam')
-    plt.plot(range(1, 10+1), hist_list[1], marker='o', color='g', label='RED')
-    plt.plot(range(1, 10+1), hist_list[2], marker='o', color='b', label='SGD')
+def _(hist_list, np, plt):
+    def get_ema(points, factor):
+        newpts = []
+        for point in points:
+            if len(newpts) == 0:
+                newpts.append(point)
+            else:
+                newpts.append(newpts[-1] * factor + point * (1 - factor))
+        return newpts
+
+    def get_avg(points, epoch=10):
+        return np.mean(np.array(points).reshape(epoch, -1), axis=1)
+    
+    """
+    x2_adahessian = np.linspace(0, 10, len(hist_list[0]))
+    x2_adam = np.linspace(0, 10, len(hist_list[1]))
+    x2_red = np.linspace(0, 10, len(hist_list[2]))
+    x2_sgd = np.linspace(0, 10, len(hist_list[3]))
+
+    plt.plot(x2_adahessian, get_ema(hist_list[0], 0.9), color='y', alpha=0.5, label='AdaHessian')
+    plt.plot(x2_adam, get_ema(hist_list[1], 0.9), color='g', alpha=0.5, label='Adam')
+    plt.plot(x2_red, get_ema(hist_list[2], 0.9), color='r', alpha=0.5, label='RED')
+    plt.plot(x2_sgd, get_ema(hist_list[3], 0.9), color='b', alpha=0.5, label='SGD')
+    """
+
+    plt.plot(range(1, 11), get_avg(hist_list[0]), marker='o', color='y', label='AdaHessian')
+    plt.plot(range(1, 11), get_avg(hist_list[1]), marker='o', color='g', label='Adam')
+    plt.plot(range(1, 11), get_avg(hist_list[2]), marker='o', color='r', label='RED')
+    plt.plot(range(1, 11), get_avg(hist_list[3]), marker='o', color='b', label='SGD')
+
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.legend()
@@ -360,7 +446,7 @@ def _(hist_list, plt):
 
 @app.cell
 def _(N_list, dict, np, vocab):
-    embed = N_list[2].list_layers[0].A.T
+    embed = N_list[3].list_layers[0].A.T
     norms = np.linalg.norm(embed, axis=1, keepdims=True)
     norm_embed = embed / norms
 
